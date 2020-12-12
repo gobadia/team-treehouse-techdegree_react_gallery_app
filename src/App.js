db4ec68eee8a032a42b4ce8fd12240971c18cd3a
gobadia <gabrielobadia@gmail.com>
2020-12-12 10:43:13 -0500

    correction

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import {
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
 import PhotoContainer from './components/PhotoContainer';
-import NotFound from './components/NotFound';
 import ErrorFourFour from './components/ErrorFourFour';
 
 function App() {
